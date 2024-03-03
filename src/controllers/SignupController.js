29fdaa8e89aaa79c69b9cf7b4282fcb3bd4202aa
Aashish Katwal <aashiskutwal5@gmail.com>
2024-03-03 20:30:21 +0545

    Created SignupController and received data from Frontend

--- src/controllers/SignupController.js
+++ src/controllers/SignupController.js
@@ -1,1 +1,10 @@
 // This file contains the code for registration.
+
+// @desc Get information from frontend.
+// @route GET /signup
+// @access public
+const registerUser = (req, res) => {
+  res.status(200).json({ salt: passwordSalt });
+};
+
+module.exports = { registerUser };
