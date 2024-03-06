51bb76a24ca8855ab8574539c001e79d4320cc1c
netrabista1 <netrabista723@gmail.com>
2024-03-06 08:08:24 -0800

    icons to svg

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/login",(req,res)=>{
+router.get("/login", (req, res) => {
   res.render("login");
 })
 module.exports = router;
