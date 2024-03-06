8769c2b81fda5e5261c31f159d8fff60e29b8132
Kashyap <kashyapshakya2@gmail.com>
2024-03-06 11:31:53 +0545

    LoginVaidation added

--- src/public/js/LoginValidation.js
+++ src/public/js/LoginValidation.js
@@ -1,14 +1,38 @@
-const passwordField = document.getElementById("password");
-const togglePassword = document.querySelector(".password-toggle-icon i");
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
 
-togglePassword.addEventListener("click", function () {
-  if (passwordField.type === "password") {
-    passwordField.type = "text";
-    togglePassword.classList.remove("fa-eye");
-    togglePassword.classList.add("fa-eye-slash");
-  } else {
-    passwordField.type = "password";
-    togglePassword.classList.remove("fa-eye-slash");
-    togglePassword.classList.add("fa-eye");
-  }
+    form.addEventListener("submit", function (event) {
+        event.preventDefault(); 
+
+        const usernameInput = document.getElementById("username");
+        const passwordInput = document.getElementById("password");
+
+        const usernameValue = usernameInput.value.trim();
+        const passwordValue = passwordInput.value.trim();
+
+        const usernamePattern = /^[a-zA-Z0-9_]+$/;
+
+        if (passwordValue === "") {
+            displayErrorMessage2("Password is required.");
+            return;
+        }
+        if (usernameValue !== "" && usernameValue.includes("@")) {
+            displayErrorMessage1("Username cannot be an email.");
+            return;
+        }
+        if (usernameValue !== "" && !usernamePattern.test(usernameValue)) {
+            displayErrorMessage1("Username can only contain letters, numbers, and underscores.");
+            return;
+        }
+        form.submit();
+    });
+
+    function displayErrorMessage1(message) {
+        const errorMessageElement = document.getElementById("username-error-msg");
+        errorMessageElement.textContent = message;
+    }
+    function displayErrorMessage2(message) {
+        const errorMessageElement = document.getElementById("pwd-error-msg");
+        errorMessageElement.textContent = message;
+    }
 });
