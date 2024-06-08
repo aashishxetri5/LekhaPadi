0c7034d05fe29198168c2755a1b74c11e65d5c82
Aashish Katwal <aashiskutwal5@gmail.com>
2024-06-08 20:35:08 +0545

    Implemented login feature

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,9 +1,59 @@
-const { use } = require("../routes");
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const User = require("../models/UserModel");
 
-const userLogin = (req, res) => {
+exports.login = async (req, res) => {
   const { username, password } = req.body;
 
-  res.status(200).json({ msg: "Okay" });
+  let dbConnection;
+  try {
+    // Connect to the database
+    dbConnection = await mongoose.connect(
+      "mongodb://localhost:27017/lekhapadi"
+    );
+
+    // Find the user with the provided username
+    const user = await User.findOne({ username });
+    if (!user) {
+      console.log("User not found");
+      return res.status(401).redirect("/login");
+    }
+
+    // Hash the provided password using crypto
+    const hash = crypto
+      .createHash("sha256")
+      .update(password + user.salt)
+      .digest("hex");
+
+    if (hash === user.password) {
+      // Passwords match, generate session and redirect to dashboard
+      req.session.user = {
+        id: user._id,
+        fullname: user.fullname,
+        username: user.username,
+        email: user.email,
+      };
+
+      return res.redirect("/");
+    } else {
+      return res.status(401).redirect("/login");
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  } finally {
+    if (dbConnection) {
+      console.log("Closing database connection");
+      await mongoose.disconnect();
+    }
+  }
 };
 
-module.exports = { userLogin };
+exports.logout = (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).render("serverError", { title: "Server Error" });
+    }
+    res.redirect("/");
+  });
+};
